4cf13f13447687f89af5bab46f55fead7ba44bdf
morganbairamkulow <dbairamkulow@mail.ru>
2022-01-14 20:49:09 +0300

    New universe field

--- Universe/settings.py
+++ Universe/settings.py
@@ -33,8 +33,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', get_random_secret_key())
 DEVELOPMENT_MODE = os.getenv('DEVELOPMENT_MODE', 'False') == 'True'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False# \
-    #os.getenv('DEBUG', 'FALSE') == 'True'
+DEBUG =os.getenv('DEBUG', 'FALSE') == 'True'
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 CORS_ORIGIN_ALLOW_ALL = True
